e573b293177542e781f125506a22c6b69952c295
jc993626 <bmcmurra77@gmail.com>
2026-06-10 14:26:01 +1000

    post video1 exercise complete, changed list indexing 0 = 1 [name_to_display - 1] as index

--- wk3_exercises/wk4_exercises/indexing_error_handling.py
+++ wk3_exercises/wk4_exercises/indexing_error_handling.py
@@ -1,19 +1,19 @@
 
 names = ["Lindsay", "Hossein", "Dmitry", "Bruce", "Alan"]
-number_of_elements = len(names)
+amount_of_names = len(names)
 is_valid_input = False
 while not is_valid_input:
     try:
         print("Enter the number for the name you wish to select")
-        print(f"[0] for {names[0]}, [1] for {names[1]}, [2] for {names[2]}, [3] for {names[3]}, [4] {names[4]}")
+        print(f"[1] for {names[0]}, [2] for {names[1]}, [3] for {names[2]}, [4] for {names[3]}, [5] {names[4]}")
         name_to_display = int(input("Enter a number:"))
-        if 0 > name_to_display >= number_of_elements :
+        if name_to_display < 0 or name_to_display > amount_of_names:
             print("Invalid selection")
         else:
             is_valid_input = True
     except ValueError:
         print("Invalid number for selection of name")
-print(f"The name you selected is {names[name_to_display]}")
+print(f"The name you selected is {names[name_to_display - 1]}")
 
 # how to have a selection of '1' be [0] index ??
 
