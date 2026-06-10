8480f202eb046157a5eb4a1fb9332d61c5891493
jc993626 <bmcmurra77@gmail.com>
2026-06-10 21:21:09 +1000

    video5 finished

---
+++ wk3_exercises/wk4_exercises/video5_equality.py
@@ -0,0 +1,28 @@
+# EQUALITY
+#x == y check if values the same
+# x is y check if variables refer to same object in memory
+# id() to test this
+#numbers = [        ]
+# things = numbers
+# new_things = [      ]
+#things == numbers          TRUE
+# things is numbers         TRUE
+# new_things == numbers    TRUE
+# new_things is numbers     FALSE
+# == check is value same
+# is , check if use same memory
+#
+#numbers = [      ]
+#things = numbers
+#things is an ALIAS of numbers
+# other = more[:] made copy, but made a slice that contains same values, so diff memory object
+# IS different variables have same value , use same memory n1 is n2 TRUE as long as number <255
+# IF LARGE NUMBER or STRING when same value uses different memory
+#
+
+
+
+
+
+
+
