5d49100a7c9dfeed80d4775560678863ef913e1e
jc993626 <bmcmurra77@gmail.com>
2026-06-10 22:19:00 +1000

    video6 finished

---
+++ wk3_exercises/wk4_exercises/Video6_more_strings.py
@@ -0,0 +1,57 @@
+# MORE STRINGS
+# text = "ABC" text[0] is A
+# SLICING
+# span of items taken from sequence
+# sequence[start:end] start to not including end like RANGE
+# can include step value  eg.  sequence[start:end:step]
+#s = "Hello and welcome to this lecture!"
+# s[0]
+# 'H'
+# s[:2]
+# 'He'
+# s[2:]
+# 'llo and welcome to this lecture!'
+#s[:-1] everything but last character
+#'Hello and welcome to this lecture'
+# text = "(variable)"
+# text[1:-1] start at 1, finish before -1
+# 'variable'
+#
+#text = "Python"
+# text[0:4]
+# 'Pyth'
+# text[4:7]
+# 'on'
+# text[0:-4]
+# 'Py'
+# text[0::2]
+# 'Pto'
+# text[2:]
+# 'thon'
+# text[::-1]   REVERSE
+#'nohtyP'
+#
+# common string methods
+# .upper(), .lower(), .title()
+# endswith(substring), startswith(substring)
+# FIND find(substring) returns lowest index, if not found returns -1
+#
+#s[s.find(' '):] starts at space to the end
+#' and welcome to this lecture!'
+# s[s.find(' ') + 1:]     start +1 after space.
+# s[:s.find(' ')]     everything upto first space
+# s.count('e') find number of e
+# 5
+# isdigit, isalpha, isupper
+# isdecimal, isdigit, isnumeric
+
+# IN look for substring in string
+# make sure dont use to check EQUALITY
+# + is concatenation for strings
+# += adds to end of string
+# ''.join([s1, s2])    JOINS two strings, s1 and s2    RECOMMENDED
+#
+#
+#
+
+
